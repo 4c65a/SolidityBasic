1c279dbbfcf20ac2be8c57698b04750e74ad46ff
Leandro Alexis Borquez <110344935+LeanBorquez@users.noreply.github.com>
2022-08-10 03:08:06 +0100

    Create IfElse.sol

---
+++ IfElse.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.3;
+
+function IfElse(uint x) public pure returns (uint){
+   if (x < 10){
+       returns 0;
+   }else if (x < 20){
+       returns 1;
+   } else {
+       returns 2;
+   }
+}
